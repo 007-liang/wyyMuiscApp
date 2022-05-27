c449f626434faf8652ab6838c533137f9ad77878
007-liang <1909802953@qq.com>
2022-05-27 15:14:51 +0800

    init

---
+++ components/sideMenuAndSearch/sideMenuAndSearch.vue
@@ -0,0 +1,23 @@
+<template>
+	<!-- 侧边菜单和搜索框 -->
+	<view :style="{ height: statusBarHight }"></view>
+	<view> 搜索 </view>
+</template>
+
+<script>
+	export default {
+		name:"sideMenuAndSearch",
+		data() {
+			return {
+				statusBarHight: 0,
+			};
+		},
+		created() {
+			this.statusBarHight = uni.getSystemInfoSync().statusBarHeight;
+		}
+	}
+</script>
+
+<style lang="less">
+
+</style>
---
+++ unpackage/dist/dev/mp-weixin/app.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports[Symbol.toStringTag] = "Module";
+var common_vendor = require("./common/vendor.js");
+if (!Math) {
+  "./pages/index/index.js";
+}
+const _sfc_main = {
+  onLaunch: function() {
+    console.log("App Launch");
+  },
+  onShow: function() {
+    console.log("App Show");
+  },
+  onHide: function() {
+    console.log("App Hide");
+  }
+};
+var App = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__file", "G:/\u6848\u4F8B/\u5C0F\u7A0B\u5E8F/\u7F51\u6613\u4E91\u97F3\u4E50/wyyMusic/App.vue"]]);
+function createApp() {
+  const app = common_vendor.createSSRApp(App);
+  return {
+    app
+  };
+}
+createApp().app.mount("#app");
+exports.createApp = createApp;
